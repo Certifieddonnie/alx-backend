d2b53e49642ff4d5af800ac3ab9caec337c70bfe
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-05-23 20:52:35 +0100

    [feat]: Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Basic Flask App """
+from flask import Flask, render_template
+from typing import Any
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() -> Any:
+    return (render_template('0-index.html'))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
